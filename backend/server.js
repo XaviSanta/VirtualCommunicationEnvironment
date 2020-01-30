585754544afda8510fd053aee7266bbfec3fc428
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-01-30 23:14:56 +0100

    Keep the user list as they connect

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-var users = [];
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+
+// List of current users connected to the webSocket
+var users = [];
 
 var server = http.createServer((req, res) => {
   console.log('Request:' + requestAnimationFrame.url);
@@ -18,8 +20,13 @@ var wsServer = new WebSocketServer({
 // WebSocket server
 wsServer.on('request', (req) => {
   var connection = req.accept(null, req.origin);
-  console.log('New WebSocket User');
-
+  
+  // Save in which position the user is in the list so we can remove easily when 
+  // they disconnect
+  var index = users.push(connection) - 1; 
+  console.log('New WebSocket User: ' + req.origin + ' index: ' + index);
+  
+  // When we recieve a message from a user:
   connection.on('message', (msg) => {
     if (msg.type === 'utf8') {
       console.log('New MEssage: ' + msg.utf8Data);
@@ -28,5 +35,7 @@ wsServer.on('request', (req) => {
 
   connection.on('close', (connection) => {
     console.log('User is gone');
+    // Remove user from the list
+    users.splice(index, 1);
   });
 });
