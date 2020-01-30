1a6804d6ed3b3ec1efeaf1c8594ec35268d00665
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-01-30 23:42:40 +0100

    Add todos

--- frontend/code.js
+++ frontend/code.js
@@ -1,9 +1,12 @@
+// TODO: Get the username that the user input before
+// Maybe save it when the user does the login and save
+// it in a globalVariable
 var userName = 'ChangeThis';
 
 $(function(){
   var input = $('#input');
   // var connection = new WebSocket('ws://ecv-etic.upf.edu:9034'); // Descomentar al server machine
-  var connection = new WebSocket('ws://127.0.0.1:9034'); // test on localhost
+  // var connection = new WebSocket('ws://127.0.0.1:9034'); // test on localhost
 
   connection.onopen = () => {
     console.log('Connection is open and ready to use');
@@ -21,8 +24,10 @@ $(function(){
       console.log('This doesnt look like valid JSON');
       return;
     }
-    console.log('Received message');
     console.log(json);
+    // TODO: Check type of message 
+    //  TODO: If its a user message: Append message in chatContainer
+    //  TODO: If its a user move: move that character to the specified location
   };
 
   input.keydown((e) => {
