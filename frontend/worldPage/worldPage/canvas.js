9b9e7da2c1203da5aee3b9c28052970506f261c6
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-02-08 01:07:48 +0100

    Restore work

---
+++ frontend/worldPage/worldPage/canvas.js
@@ -0,0 +1,175 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+var person = new Image();
+person.src = '../images/man1-spritesheet.png';
+characters = [
+  '../images/man1-spritesheet.png', 
+  '../images/man2-spritesheet.png', 
+  '../images/man3-spritesheet.png',
+  '../images/man4-spritesheet.png',
+  '../images/woman1-spritesheet.png',
+  '../images/woman2-spritesheet.png',
+  '../images/woman3-spritesheet.png',
+  '../images/woman4-spritesheet.png',
+];
+var w = 32; //sprite width
+var h = 64; //sprite height
+var idle = [16];
+var talking = [16,17];
+var walking = [2,3,4,5,6,7,8,9];
+var currentFrame = 0;
+var points;
+var lastX = 0;
+var lastY = 0;
+
+// Send new coordinates of the user move
+canvas.addEventListener('click', function(e) {
+  lastX = positions[username].posX;
+  lastY = positions[username].posY;
+
+  points = linePoints(lastX, lastY, e.clientX, e.clientY, 60);
+  currentFrame = 0;
+  
+  lastX = e.clientX;
+  lastY = e.clientY;
+  animate();
+});
+
+function animate() {
+  var point = points[currentFrame++];
+
+  // Calculate direction
+  let dir = calculateDirection();
+
+  connection.send(JSON.stringify({type: 'position', posX: point.x, posY: point.y, direction: dir}));
+
+  // refire the timer until out-of-points
+  if (currentFrame < points.length) {
+      timer = setTimeout(animate, 1000 / 60);
+  }
+
+  // Send that the user has stopped
+  if(currentFrame == points.length) {
+    connection.send(JSON.stringify({type: 'position', posX: point.x, posY: point.y, direction: 'idle'}));
+  }
+}
+
+function calculateDirection() {
+  let init = points[0];
+  let end = points[60];
+  let dx = init.x - end.x;
+  let dy = init.y - end.y;
+
+  if(dy > 0 && dy > Math.abs(dx)) return 'up';
+  if(dy < 0 && Math.abs(dy) >  Math.abs(dx)) return 'down';
+  if(dx < 0) return 'right';
+  if(dx > 0) return 'left';
+}
+
+var last = performance.now();
+
+function loop() {
+  draw();
+  var now = performance.now();
+  var elapsed_time = (now-last) / 1000;
+  last = now;
+  update(elapsed_time);
+  requestAnimationFrame( loop );
+}
+
+//start loop
+loop();
+
+function draw() {
+  var parent = canvas.parentNode;
+  var rect = parent.getBoundingClientRect();
+  canvas.width = rect.width;
+  canvas.height = rect.height;
+  // TODO: draw the 'floor'
+  drawUsers();
+}
+
+function update() { // TODO: This maybe we can do it as the last thing
+  // TODO: Update the user list position if needed
+  // TODO: Update the user characters spritesheet if they walk
+}
+
+// -------------------------------------------------------------------------------
+function drawUsers() {
+  users = Object.keys(positions);
+  users.forEach(u => {
+    drawUser(u);
+  });
+}
+
+function linePoints(x1, y1, x2, y2, frames) {
+  var dx = x2 - x1;
+  var dy = y2 - y1;
+  var length = Math.sqrt(dx * dx + dy * dy);
+  var incrementX = dx / frames;
+  var incrementY = dy / frames;
+  var a = new Array();
+
+  a.push({
+      x: x1,
+      y: y1
+  });
+  for (var frame = 0; frame < frames - 1; frame++) {
+      a.push({
+          x: x1 + (incrementX * frame),
+          y: y1 + (incrementY * frame)
+      });
+  }
+  a.push({
+      x: x2,
+      y: y2
+  });
+  return (a);
+}
+
+
+function drawUser(u) {
+  let x = positions[u].posX;
+  let y = positions[u].posY;
+  let dir = positions[u].dir || 'idle';
+  let anim; 
+  if(dir === 'idle') anim = idle;
+  else {
+    let i;
+    if(dir === 'right') i = 0;
+    if(dir === 'down')  i = 1;
+    if(dir === 'left')  i = 2;
+    if(dir === 'up')    i = 3;
+    anim = walking.map(x => x + 16 * i );
+  }
+  renderAnimation(ctx, person, anim, x-w, y - 2*h, 1.5, 0, false);
+
+  // Draw Username
+  ctx.textAlign = 'center';
+  ctx.fillText(u, x, y - 0.3*h);
+
+  // Draw last Message
+  ctx.fillText(messageUser[u] || '', x, y - 2*h);
+}
+
+// Print sprite 
+function renderAnimation( ctx, image, anim, x, y, scale, offset, flip ) {
+  offset = offset || 0;
+  var t = Math.floor(performance.now() * 0.001 * 10);
+  renderFrame( ctx, image, anim[ t % anim.length ] + offset, x,y,scale,flip);
+}
+
+function renderFrame(ctx, image, frame, x, y, scale, flip) {
+  scale = scale || 1;
+  var num_hframes = image.width / w;
+  var xf = (frame * w) % image.width;
+  var yf = Math.floor(frame / num_hframes) * h;
+  ctx.save();
+  ctx.translate(x,y);
+  if( flip ) {
+    ctx.translate(w*scale,0);
+    ctx.scale(-1,1);
+  }
+  ctx.drawImage( image, xf,yf,w,h, 0,0,w*scale,h*scale );
+  ctx.restore();
+}
---
+++ frontend/worldPage/worldPage/world.js
@@ -0,0 +1,100 @@
+$('#initial-screen').css('display', 'none');
+connection.onmessage = (msg) => manageConnectionMesssage(msg);
+connection.onerror = (err) => manageConnectionError(err);
+
+$('.chat-form input').keydown((e) => {
+  if(e.key === 'Enter') {
+    sendInputMessage();
+  }
+});
+
+function sendInputMessage() {
+  var content = $('.chat-form input').val();
+  if(isValidString(content, ['<','>'])) {
+    connection.send(JSON.stringify({type: 'message', content: content}));
+  }
+
+  clearInput();
+  setFocusMessageInput();
+}
+
+function manageConnectionMesssage(msg) {
+  var obj = JSON.parse(msg.data);
+  console.log('received obj', obj);
+
+  if(obj.type === 'positions') {
+    positions = obj.data;
+  }
+
+  if(obj.type === 'message') {
+    appendMessage(obj.data);
+  }
+
+  if(obj.type === 'position') {
+    // Update the user position with the new data
+    let author = obj.data.author;
+    let posX = obj.data.posX;
+    let posY = obj.data.posY;
+    let dir = obj.data.direction;
+
+    positions[author] = {posX, posY, dir};
+    console.log('Updatedpositions', positions);
+  }
+
+  if(obj.type === 'closeConnection') {
+    // Remove user from the object position se we stop drawing him on the canvas
+    let username = obj.data;
+    delete positions[username];
+  }
+}
+
+function manageConnectionError(err) {
+  console.log('An error ocurred' + err);
+}
+
+// MESSAGE LIST
+var messageList = [];
+var messageUser = {};
+function appendMessage(msg) {
+  // Add to list of messages
+  messageList.push(msg);
+  messageUser[msg.author] = msg.content;
+
+  // Create the elements to append a message
+  let messageListContainer = document.getElementById('container-messages');
+  let messageContainer = document.createElement('div');
+  let usernameDiv = document.createElement('div');
+  let textMessageP = document.createElement('p');
+  
+  // Set the attributes
+  usernameDiv.innerHTML = msg.author;
+  // usernameDiv.style.color = getColorById(msg.id);
+  textMessageP.innerHTML = msg.content;
+  
+  // If its my message -> set class to our message, else set class to message
+  msg.author === username ? 
+    messageContainer.className = 'our message-container' :
+    messageContainer.className = 'message-container';
+    
+  usernameDiv.className = 'message-username';
+  
+  // Append the elements
+  messageListContainer.appendChild(messageContainer);
+  messageContainer.appendChild(usernameDiv);
+  messageContainer.appendChild(textMessageP);
+
+  // scroll bottom
+  scrollElementToBottom(messageListContainer);
+}
+
+function scrollElementToBottom(element) {
+  element.scrollTop = element.scrollHeight;
+}
+
+function setFocusMessageInput(){
+  document.getElementById("write-message").focus();
+}
+
+function clearInput() {
+  document.getElementById('write-message').value = '';
+}
